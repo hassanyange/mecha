e1eab48e883770b645c7c8d4dcc7f21ac04ade82
Pipsson <juliusmallya8420@gmail.com>
2022-08-30 10:16:00 +0300

    config

--- mechaworld/settings.py
+++ mechaworld/settings.py
@@ -132,6 +132,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
-
+DATABASES['default'] =  dj_database_url.config()
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 django_heroku.setting(locals())
