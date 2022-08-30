4279b4b24de560f0be839584ca109a4f23d1c78b
Pipsson <juliusmallya8420@gmail.com>
2022-08-30 11:47:18 +0300

    changes

--- mechaworld/settings.py
+++ mechaworld/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-9&(axdvc51pv3#3v2q-1ds^&ngh2$3&)0bjszq@n(%x!l84nuh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*','https://mechaworld.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
