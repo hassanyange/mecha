e70d543beaecca7387d3bee593ea57085cae9f84
Pipsson <juliusmallya8420@gmail.com>
2022-08-30 11:24:08 +0300

    m

--- mechaworld/settings.py
+++ mechaworld/settings.py
@@ -128,7 +128,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
     ]
 MEDIA_URL ='/img/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+TATICFILES_STORAGE ='whitenoise.storage.CompressedManifestStaticFilesStrorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
